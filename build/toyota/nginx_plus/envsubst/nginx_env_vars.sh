11aa4869f45aae1a7ccabacd9c6329908801458b
elanadmin <rajesh.moturi@elantecs.com>
2020-11-22 13:11:28 -0600

    latest Working Updates.

--- build/toyota/nginx_plus/envsubst/nginx_env_vars.sh
+++ build/toyota/nginx_plus/envsubst/nginx_env_vars.sh
@@ -4,13 +4,13 @@
 
 set -eu
 
-cp -rp /etc/nginx/conf.d/default.conf /tmp/default.conf
+cp -rp /etc/nginx/nginx.conf /tmp/nginx.conf
 cp -rp /usr/share/nginx/html/index.html /tmp/index.html
-envsubst "$(env | awk -F = '{printf " $%s", $1}')" < /tmp/default.conf > /tmp/default.conf.rendered
+envsubst "$(env | awk -F = '{printf " $%s", $1}')" < /tmp/nginx.conf > /tmp/nginx.conf.rendered
 envsubst "$(env | awk -F = '{printf " $%s", $1}')" < /tmp/index.html > /tmp/index.html.rendered
-mv /tmp/default.conf.rendered /etc/nginx/conf.d/default.conf
+mv /tmp/nginx.conf.rendered /etc/nginx/nginx.conf
 mv /tmp/index.html.rendered /usr/share/nginx/html/index.html
-rm /tmp/default.conf /tmp/index.html
+rm /tmp/nginx.conf /tmp/index.html
 
 exec "$@"
 
